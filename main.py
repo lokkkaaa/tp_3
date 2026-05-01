9ba75af434a73ddcb52ca0cff91e6038e2b92fdf
kotzuken <you@example.com>
2026-05-01 22:33:13 +0300

    добавление обработки кнопки меню

---
+++ main.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+from vika import vika_module
+from polya import run_polya
+layout = [
+    [sg.Button('Данные о пробежках', key='-KAT-', font='Helvetica 16', size=(60, 2))],
+    [sg.Button('Данные о численности населения', key='-POLYA-', font='Helvetica 16', size=(60, 2))],
+    [sg.Button('Данные о заболеваниях', key='-VIKA-', font='Helvetica 16',size=(60, 2))]
+]
+window = sg.Window('Главное меню', layout, size=(500,300))
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    if event == '-VIKA-':
+        vika_module()
+    if event == "-POLYA":
+        run_polya()
+
+
+
+window.close()
