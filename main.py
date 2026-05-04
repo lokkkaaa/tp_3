98f66d5f79c34c91a6d3516a707486b42c388195
lokka <lokij2967@gmail.com>
2026-05-04 19:40:35 +0300

    добавлен обработчик кнопки для файла kat_tp3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from vika import vika_module
 from polya import run_polya
+import Kat_tp3
 layout = [
     [sg.Button('Данные о пробежках', key='-KAT-', font='Helvetica 16', size=(60, 2))],
     [sg.Button('Данные о численности населения', key='-POLYA-', font='Helvetica 16', size=(60, 2))],
@@ -15,7 +16,8 @@ while True:
         vika_module()
     if event == "-POLYA":
         run_polya()
-
-
-
+    if event == '-KAT-':
+        window.hide()
+        Kat_tp3.run()
+        window.un_hide()
 window.close()
